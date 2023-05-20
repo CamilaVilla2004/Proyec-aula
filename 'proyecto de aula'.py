824d342180c0106852dba8445f8e6f3eaa2f6869
Valtovardenamjoon <134118353+valtovardenmajoon@users.noreply.github.com>
2023-05-20 07:07:54 -0500

    Add files via upload

---
+++ 'proyecto de aula'.py
@@ -0,0 +1,32 @@
+'proyecto de aula'
+print('PRMAPC: PLATAFORMA DE REPORTES PARA LA MEJORA DEL ALUMBRADO PÚBLICO DE LA CIUDAD DE CARTAGENA')
+print('--------------------------------------------------------------------')
+# DATOS PREDETERMINADOS
+correo = 'valt@correo.com'
+contrasena = 1234
+
+#menu
+while True:
+    print("Menú:")
+    print("1. Inicio de sesión")
+    print("2. Registrarse")
+    
+    opcion = input("Ingrese el número de la opción deseada: ")
+
+    if opcion == "1":
+        print("Digite su correo y contraseña")
+        if opcion == "1":
+            correo1 = input(str("Ingrese su correo electrónico: "))
+            contrasena1 = input(str("Ingrese su contraseña: "))
+            if correo1 == correo and contrasena1 == contrasena:
+                print("Inicio de sesión exitoso!")
+        else:
+            print("Correo o contraseña incorrectos. Inténtelo nuevamente.")
+            
+
+    elif opcion == "2":
+        print("Ha seleccionado la opción 2")
+        # Coloca aquí el código correspondiente a la opción 2
+
+    else:
+        print("Opción inválida. Por favor, ingrese un número válido.")
